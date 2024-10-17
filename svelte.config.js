01a208832986de6c4850778d4cd76086fd8b78ba
Anthony <algarza1996@gmail.com>
2024-10-17 12:52:19 -0500

    put config back

--- svelte.config.js
+++ svelte.config.js
@@ -18,7 +18,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? process.env.BASE_PATH : 'build'
+			base: process.argv.includes('dev') ? 'build' : process.env.BASE_PATH
 		}
 	}
 };
