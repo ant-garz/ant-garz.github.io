b33c4fe6ada99b26469b99b358a1ae900f19ca5d
Anthony <algarza1996@gmail.com>
2024-10-17 12:49:34 -0500

    add dark mode updates + test svelte config update

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-	import { Nav,  NavLink } from '@sveltestrap/sveltestrap';
+	import { Nav, NavLink } from '@sveltestrap/sveltestrap';
 	import ToggleDarkMode from '../components/ToggleDarkMode.svelte';
-  	import { base } from '$app/paths';
-</script>
-
-<Nav theme="auto">
-	<NavLink href="{base}/" active>Home</NavLink>
-	<NavLink href="{base}/projects">Projects</NavLink>
-	<ToggleDarkMode>
-		Toggle dark mode
-	</ToggleDarkMode>
-  </Nav>
+	import { base } from '$app/paths';
+  </script>
+  
+  <div class="position-relative">
+	<Nav theme="auto" class="d-flex justify-content-between">
+	  <div  class="d-flex">
+		<NavLink href="{base}/" active>Home</NavLink>
+		<NavLink href="{base}/projects">Projects</NavLink>
+	  </div>
+	  <div class="position-absolute top-0 end-0 p-2">
+		<ToggleDarkMode />
+	  </div>
+	</Nav>
+  </div>
--- src/components/ToggleDarkMode.svelte
+++ src/components/ToggleDarkMode.svelte
@@ -3,14 +3,21 @@
 import { onMount } from 'svelte';
 import { Button } from '@sveltestrap/sveltestrap';
 
+
+let darkMode = false;
+
 onMount(() => {
   const storedValue = localStorage.getItem('dark-mode');
   if (storedValue) {
     // Do something with the value
     console.log('Value from localStorage:', storedValue);
-    if(!document.body.classList.contains("dark-mode")){
+    if(!document.body.classList.contains("dark-mode") && storedValue === "1"){
         console.log('dark mode')
         document.body.classList.add('dark-mode');
+        // since we add this here and we toggle this here
+        darkMode = true;
+    }else{
+        darkMode = true;
     }
   }
 });
@@ -20,12 +27,44 @@ function toggle() {
 
     if(localStorage.getItem("dark-mode")){
         localStorage.removeItem("dark-mode")
+        darkMode = false;
     } else{
         localStorage.setItem("dark-mode", "1")
+        darkMode = true;
     }
 }
 
 </script>
-<Button on:click={toggle} size="sm">
-    <slot/>
-</Button>
+{#if darkMode === true}
+    <Button on:click={toggle}
+    class="btn btn-dark"
+    active={false}
+    block={false}
+    close={false}
+    color="dark"
+    disabled={false}
+    href=""
+    outline={false}
+    size="md"
+    value=""
+    >
+        <slot/>
+        <i class="bi bi-moon"></i>
+    </Button>
+{:else}
+    <Button on:click={toggle}
+    class="btn btn-light"
+    active={false}
+    block={false}
+    close={false}
+    color="light"
+    disabled={false}
+    href=""
+    outline={false}
+    size="md"
+    value=""
+    >
+        <slot/>
+        <i class="bi bi-brightness-high"></i>
+    </Button>
+{/if}
