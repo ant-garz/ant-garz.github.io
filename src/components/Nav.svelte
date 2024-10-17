4ae9e7ce24eaaa4d4f7b43d6cdb143dc66594aff
Anthony <algarza1996@gmail.com>
2024-10-17 10:27:33 -0500

    add base path to hrefs , update readme

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import { Nav,  NavLink } from '@sveltestrap/sveltestrap';
 	import ToggleDarkMode from '../components/ToggleDarkMode.svelte';
+  	import { base } from '$app/paths';
 </script>
 
 <Nav theme="auto">
-	<NavLink href="/" active>Home</NavLink>
-	<NavLink href="/projects">Projects</NavLink>
+	<NavLink href="{base}/" active>Home</NavLink>
+	<NavLink href="{base}/projects">Projects</NavLink>
 	<ToggleDarkMode>
 		Toggle dark mode
 	</ToggleDarkMode>
