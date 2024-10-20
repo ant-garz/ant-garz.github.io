1e73e0fb610d6945135bf0faeaa6bff5fc7e9e2c
Anthony <algarza1996@gmail.com>
2024-10-20 15:12:25 -0500

    add music section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,21 +3,31 @@
     import Nav from "../components/Nav.svelte";
 </script>
 
-
 <Nav></Nav>
 
 <container>
     <h1 class="mx-auto px-5 mt-3">Hello <Icon name="triangle-half" /></h1>
     <section class="mx-auto p-5">
-        <h2>About me</h2>
+        <h2>Professional Background</h2>
+        <p>My name is Anthony Garza.</p>
         <p>With professional experience as both a Software QA Analyst and a Software Developer, I bring hands-on knowledge of agile methodologies and the software development life cycle.</p>
         <p>Outside of work, my passion for technology continues to drive me to learn more. I enjoy working on personal projects with Arduino micro controllers and Raspberry Pi computers, which helps me stay enthusiastic about my field and current with emerging technologies.</p>
 
-        <h3>Professional experience</h3>
+        <h3>Experience</h3>
         <ul>
             <li>Software Development</li>
             <li>Software Quality Assurance</li>
         </ul>
+    </section>
+
+    <section class="mx-auto p-5">
+        <h2>Personal Background</h2>
+        <h3>Music</h3>
+        <p>I enjoy music and enjoy spending time playing songs on guitar and bass.</p>
+        <p>Here are some songs that I have been listening to recently:</p>
+        <div class="mx-auto w-75">
+            <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/37i9dQZF1EpsmOwbl4tnBg?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        </div>
     </section>
 </container>
 
