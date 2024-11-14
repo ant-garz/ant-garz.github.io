d02bb795d8a0c13755c4b6a8efb6838833ed4dc4
Anthony <algarza1996@gmail.com>
2024-11-14 14:35:40 -0600

    remove media queries, update svelte, styling updates

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -8,12 +8,11 @@
     CardText,
     CardTitle  } from "@sveltestrap/sveltestrap";
 	import Nav from "../../components/Nav.svelte";
-    import MediaQuery from "../../components/MediaQuery.svelte";
     import spaceInk from '$lib/assets/space-ink.jpg';
 
 
     import { theme } from '../../utilities/themeStore';
-  import { onMount } from "svelte";
+    import { onMount } from "svelte";
 
     let currentTheme:string = 'auto'; // default
     // Subscribe to the theme store
@@ -46,62 +45,20 @@
             <p>My projects reflect a range of skills, focusing on creating functional applications and software solutions. I enjoy solving problems and find joy in the process of programming.</p>
             <p>A goal of mine is to create meaningful, impactful work that benefits others. However, sometimes creating projects just for fun is a great way to explore new ideas and enhance my skills without the pressure of expectations. Take a look at my work to see my approach and capabilities.</p>
         </div>
-        <div class="mx-auto">
-            <!-- default -->
-            <MediaQuery query="(min-width: 1281px)" let:matches>
-                {#if matches}
-                    <div class="container">
-                        <!-- space ink card -->
-                        <Card class="mx-auto mx-auto w-50" theme={currentTheme}>
-                            <CardHeader>
-                                <CardTitle>Space Ink</CardTitle>
-                            </CardHeader>
-                            <CardBody>
-                                <Image class="img-fluid mx-auto w-75" fluid alt="space ink example image" src={spaceInk}></Image>
-                                <CardText>A project that shows the NASA astronomy picture of the day and displays it on an e-paper module connected to a raspberry pi</CardText>
-                                <Button on:click={goToSpaceInk}>view</Button>
-
-                            </CardBody>
-                        </Card>
-                    </div>
-                {/if}
-            </MediaQuery>
-            <!-- tablet -->
-            <MediaQuery query="(min-width: 481px) and (max-width: 1280px)" let:matches>
-                {#if matches}
-                <div class="container">
-                    <!-- space ink card -->
-                    <Card theme={currentTheme}>
-                        <CardHeader>
-                            <CardTitle>Space Ink</CardTitle>
-                        </CardHeader>
-                        <CardBody>
-                            <Image fluid alt="space ink example image" src={spaceInk}></Image>
-                            <CardText>A project that shows the NASA astronomy picture of the day and displays it on an e-paper module connected to a raspberry pi</CardText>
-                            <Button on:click={goToSpaceInk}>view</Button>
-                        </CardBody>
-                    </Card>
-                </div>
-                {/if}
-            </MediaQuery>
-            <!-- mobile -->
-            <MediaQuery query="(max-width: 480px)" let:matches>
-                {#if matches}
-                <div class="container">
-                    <!-- space ink card -->
-                    <Card theme={currentTheme}>
-                        <CardHeader>
-                            <CardTitle>Space Ink</CardTitle>
-                        </CardHeader>
-                        <CardBody>
-                            <Image fluid alt="space ink example image" src={spaceInk}></Image>
-                            <CardText>A project that shows the NASA astronomy picture of the day and displays it on an e-paper module connected to a raspberry pi</CardText>
-                            <Button on:click={goToSpaceInk}>view</Button>
-                        </CardBody>
-                    </Card>
-                </div>
-                {/if}
-            </MediaQuery>
+        <div class="container ">
+            <div >
+                <!-- space ink card -->
+                <Card class="mx-auto w-75" theme={currentTheme}>
+                    <CardHeader>
+                        <CardTitle>Space Ink</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <Image class="img-fluid mx-auto" fluid alt="space ink example image" src={spaceInk}></Image>
+                        <CardText>A project that shows the NASA astronomy picture of the day and displays it on an e-paper module connected to a raspberry pi.</CardText>
+                        <Button on:click={goToSpaceInk}>view</Button>
+                    </CardBody>
+                </Card>
+            </div>
         </div>
     </section>
 </container>
