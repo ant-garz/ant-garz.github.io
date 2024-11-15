813dbb724df06ec7711987503e74f34379fd8f84
Anthony <algarza1996@gmail.com>
2024-11-14 22:06:13 -0600

    update arrow animation, fix spotify iframe display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,15 +22,20 @@
     ];
     let activeIndex = 0;
     import { theme } from '../utilities/themeStore';
+    import { browser } from "$app/environment";
     let currentTheme:string = 'auto'; // default
     // Subscribe to the theme store
     theme.subscribe(value => {
         currentTheme = value;
     });
 
+    // used for dynamic canvas as hero image
     let canvas: HTMLCanvasElement;
-  let ctx: CanvasRenderingContext2D;
+    let ctx: CanvasRenderingContext2D;
 
+    // used for updating ratio on spotify iframe div
+    let width : number;
+    let isMobile:boolean = false;
   // Helper function to generate a random color in RGB format, with controlled brightness
   function getRandomColor(): string {
     const h = Math.floor(Math.random() * 360); // Random Hue (0 to 360)
@@ -147,11 +152,13 @@
     requestAnimationFrame(animate); // Request the next frame
   }
 
-    // Set the initial theme on mount
-    onMount(() => {
-        document.body.className = currentTheme;
-        ctx = canvas.getContext("2d") as CanvasRenderingContext2D ;
-        if (ctx) {
+  // Set the initial theme on mount
+  onMount(() => {
+    document.body.className = currentTheme;
+    window.addEventListener('resize', handleResize);
+
+    ctx = canvas.getContext("2d") as CanvasRenderingContext2D ;
+    if (ctx) {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
 
@@ -161,7 +168,25 @@
 
       requestAnimationFrame(animate); // Start the animation loop
     }
-    });
+  });
+
+  onDestroy(() => {
+    if(browser){
+      window.removeEventListener('resize', handleResize);
+    }
+  });
+
+  if(browser){
+  // Reactive variable for window width
+  width = window.innerWidth;
+  // You can define a mobile breakpoint (e.g., 768px)
+  isMobile = width <= 768;
+  }
+
+  // Update width on resize
+  const handleResize = () => {
+  width = window.innerWidth;
+  };
 
 </script>
 
@@ -176,14 +201,14 @@
         <!-- Text content that will be displayed above the canvas -->
         <div class="hero-text">
             <h1 class="mx-auto px-5 mt-3">Hello, my name is Anthony Garza <Icon name="person-workspace" /></h1>
-            <div bind:this={scrollArrow} class="scroll-arrow">
-                <h1 class="mt-3"><Icon name="arrow-down-circle" /></h1>
+            <div class="scroll-arrow">
+                <h1 class="mt-5"><Icon name="arrow-down-circle" /></h1>
             </div>
         </div>
     </div>
 
-    <section class="mx-auto p-5">
-        <h2>Professional Background</h2>
+    <section class="mx-auto px-5">
+        <h2 class="mt-2">Professional Background</h2>
         <p>With professional experience as both a Software QA Analyst and a Software Developer, I bring hands-on knowledge of agile methodologies and the software development life cycle.</p>
         <p>Outside of work, my passion for technology continues to drive me to learn more. I enjoy working on personal projects with Arduino micro controllers and Raspberry Pi computers, which helps me stay enthusiastic about my field and current with emerging technologies.</p>
 
@@ -201,15 +226,16 @@
         </div>
     </section>
 
-    <section class="mx-auto p-5">
+    <section class="mx-auto px-5">
         <h2>Personal Background</h2>
         <div class="mx-auto">
             <h3>Music</h3>
             <p>I enjoy music and enjoy spending time playing songs on guitar and bass.</p>
             <p>Here are some songs that I have been listening to recently:</p>
-            <div class="container mt-4">
-                <div class="ratio ratio-16x9 mx-auto">
+            <div class="mt-4">
+                <div class="ratio {isMobile ? 'ratio-1x1' : 'ratio-16x9'} mx-auto">
                     <iframe
+                        title="on repeat playlist embed"
                         style="border-radius: 12px"
                         src="https://open.spotify.com/embed/playlist/37i9dQZF1EpsmOwbl4tnBg?utm_source=generator&theme=0"
                         frameborder="0"
@@ -222,33 +248,31 @@
         </div>
     </section>
 
-    <section class="mx-auto p-5">
-        <div>
-            <h3>Photography</h3>
-            <p>I also enjoy photography as a hobby. Here are some photos I took recently.</p>
-            <div class="container">
-                <Carousel {items} bind:activeIndex>
-                    <CarouselIndicators bind:activeIndex {items} />
-                    <div class="carousel-inner">
-                        {#each items as item, index}
-                        <CarouselItem bind:activeIndex itemIndex={index}>
-                          <div class="position-relative d-flex justify-content-center align-items-center">
-                            <!-- Image container with Bootstrap classes to maintain aspect ratio -->
-                            <enhanced:img
-                              src={item}
-                              alt="photo {index + 1}"
-                              class="img-fluid"
-                              sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
-                            />
-                          </div>
-                        </CarouselItem>
-                      {/each}
+    <section class="mx-auto px-5">
+      <h3>Photography</h3>
+      <p>I also enjoy photography as a hobby. Here are some photos I took recently.</p>
+      <div class="container">
+          <Carousel {items} bind:activeIndex>
+              <CarouselIndicators bind:activeIndex {items} />
+              <div class="carousel-inner">
+                  {#each items as item, index}
+                  <CarouselItem bind:activeIndex itemIndex={index}>
+                    <div class="position-relative d-flex justify-content-center align-items-center">
+                      <!-- Image container with Bootstrap classes to maintain aspect ratio -->
+                      <enhanced:img
+                        src={item}
+                        alt="photo {index + 1}"
+                        class="img-fluid"
+                        sizes="(min-width:1920px) 1280px, (min-width:1080px) 640px, (min-width:768px) 400px"
+                      />
                     </div>
-                    <CarouselControl direction="prev" id="prev" bind:activeIndex {items} />
-                    <CarouselControl direction="next" id="next" bind:activeIndex {items} />
-                </Carousel>
-            </div>
-        </div>
+                  </CarouselItem>
+                {/each}
+              </div>
+              <CarouselControl direction="prev" id="prev" bind:activeIndex {items} />
+              <CarouselControl direction="next" id="next" bind:activeIndex {items} />
+          </Carousel>
+      </div>
     </section>
 </container>
 
@@ -297,7 +321,7 @@
     bottom: 30px; /* Position it towards the bottom */
     font-size: 2rem;
     color: white;
-    animation: bounce 1.5s ease-in-out infinite; /* Apply animation */
+    animation: bounce 2.5s ease-in-out infinite; /* Apply animation */
     cursor: pointer;
   }
 
